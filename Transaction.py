7bf787d24ca1a810931dbf61bd3849c50b585f78
Tijmen-vd-Ban <93472423+Tijmen-vd-Ban@users.noreply.github.com>
2021-10-31 13:20:52 +0100

    Create Transaction.py

---
+++ Transaction.py
@@ -0,0 +1,6 @@
+# I made this class so that i could include the subject of the transaction
+
+class Transaction:
+    def __init__(self, amount, subject):
+        self.amount = amount
+        self.subject = subject
